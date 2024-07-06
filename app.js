c963c2c612822429b67865c0d83e6cf8f8c21d13
jawadmehmood <jawadam5678@gmail.com>
2024-07-07 03:11:30 +0500

    JS Alert Completed

---
+++ app.js
@@ -0,0 +1,21 @@
+// Assignment 1
+
+alert('Welcome to my website!');
+
+// Assignment 2
+
+alert('Error! Please enter a valid password');
+
+// Assignment 3
+
+alert('Welcome to JS Land...\nHappy Coding!');
+
+// Assignment 4
+
+alert('Welcome to JS Land...');
+alert('Happy Coding!');
+
+// Assignment 5
+
+alert("Hello... I can run JavaScript through my browser's console");
+console.log("Hello... I can run JavaScript through my browser's console");
